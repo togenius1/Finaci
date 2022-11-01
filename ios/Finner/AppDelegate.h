eeb9deaeb3008d670545b0e3e5b782a924f61660
Todsaporn Laosa <togenius1@gmail.com>
2022-11-01 18:19:33 +0700

    udated

--- ios/Finner/AppDelegate.h
+++ ios/Finner/AppDelegate.h
@@ -3,8 +3,8 @@
 #import "RNAppAuthAuthorizationFlowManager.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, RNAppAuthAuthorizationFlowManager>
-@property(nonatomic, weak)id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
 
 @property (nonatomic, strong) UIWindow *window;
+@property(nonatomic, weak)id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
 
 @end
